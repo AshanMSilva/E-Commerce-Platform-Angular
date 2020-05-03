33e25dc2e845fcca68c0ac729820104e4c94720b
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2020-05-03 20:11:47 +0530

    initial commit with homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopCategoryComponent } from './pages/top-category/top-category.component';
+import { TopproductComponent } from './pages/topproduct/topproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    FooterComponent,
+    TopCategoryComponent,
+    TopproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
